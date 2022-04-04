f79c9a63682b79ce4dd4cbc26103df0ca5f96dbf
Ashutosh Bhardwaj <ashutoshbhardwaj@Ashutoshs-MacBook-Pro.local>
2022-04-04 17:02:05 +0530

    added update order and order list api

--- order/urls.py
+++ order/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from order.views import OrderCreateView
+from order.views import OrderCreateView, OrderListView, OrderUpdateView
 
 urlpatterns = [
     path('', OrderCreateView.as_view(), name='create_order'),
+    path('list', OrderListView.as_view(), name='list_order'),
+    path('<str:id>', OrderUpdateView.as_view(), name='list_order'),
 ]
--- order/views.py
+++ order/views.py
@@ -20,3 +20,38 @@ class OrderCreateView(generics.CreateAPIView):
         data = self.serializer_class(order_obj).data
         print('returning response', data)
         return send_response(response_code=201, data=data, message='success', error=None)
+
+
+class OrderListView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.filter(is_deleted=False)
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def list(self, request, *args, **kwargs):
+        print('request received to get all restaurant ')
+        print(request.GET)
+        query_set = self.get_queryset().filter(**{k: v[0] for k, v in request.GET.items()})
+        data = self.serializer_class(query_set.all(), many=True).data
+        amounts = {}
+        [amounts.update({item['status']: amounts.get(item['status'], 0) + float(item['amount'])}) for item in data]
+        print('returning response', data)
+        return send_response(response_code=200, data={'orders': data,
+                                                      'count': query_set.count(),
+                                                      'amount': amounts}, message='success', error=None)
+
+
+class OrderUpdateView(generics.UpdateAPIView):
+    serializer_class = OrderSerializer
+    queryset = Order.objects.filter(is_deleted=False).all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    lookup_url_kwarg = 'id'
+
+    def put(self, request, *args, **kwargs):
+        print('request received to update order')
+        order = self.get_object()
+        order.status = request.data['status'] if 'status' in request.data else order.status
+        order.save()
+        data = self.serializer_class(order).data
+        return send_response(response_code=201, data=data, message='success', error=None)
